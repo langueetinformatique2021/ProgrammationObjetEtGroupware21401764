d5870c11428fff001062403bd092232cadda2857
parallels <parallels@fedora-40-arm64.shared>
2024-12-05 11:15:35 +0100

    zrx

--- atelier07/Calendar2.java
+++ atelier07/Calendar2.java
@@ -1,7 +1,4 @@
-//Créer la classe Calendar2 dérivée de Calendar implémentant  attribut cours et trois nouvelles est de type boolean et contient vrai si l’objet est cré
-//méthode creation() affichera la date et l’heure de la création de l’objet., la 
-//afficher() la date et l’heure courante, la méthode duree() le nombre de secondes depuis la 
-//création de l’objet.
+
 
 //创建一个名为 Calendar2 的类，继承自 Calendar 类。该类需要包含以下功能：
 //属性 cours：是一个布尔类型的属性，表示对象是否已经创建，如果已创建则值为 true，否则为 false。
@@ -19,9 +16,16 @@ import java.util.TimeZone;
 
 
 public class Calendar2 extends GregorianCalendar {
-	private boolean cours;
-
+	//你继承了 GregorianCalendar 类，并在类中定义了新的方法。
+	//类中的属性 cours 和方法 creation(), afficher(), duree()
+}
+	private boolean cours; //私有属性
+	//使用 private 修饰符来定义属性（或字段）是一种常见的封装技术，
+//**私有属性（private）**确保只能通过类的内部代码访问这些属性，而外部代码无法直接访问和修改这些属性。这可以防止错误地修改对象的状态，确保对象的完整性和一致性。
+//比如，你可能希望限制某个属性（如 cours）的值只能在特定条件下被修改，这样可以避免外部代码直接修改属性，从而影响类的状态。
 	public void creation(){
+	//**方法（函数）**通常用于定义类的行为，供外部代码访问。通过公开（public）方法，外部代码可以与对象进行交互。
+	//afficher() 方法可能用于显示当前的日期和时间，duree() 方法可能用于计算时间差。这些方法需要公开，以便外部代码能够调用它们。如果你将方法设置为 private，则外部无法访问该方法，这将导致外部代码无法使用该类的功能。通常，我们将方法设置为 public，是因为方法是对象和外部代码之间交互的桥梁。
 	}
 	public void afficher() {
 	}
